d8262ab96c0ac8fb213922f82675bfaaec4d7cc6
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-07-10 21:57:58 -0500

    Memoization y Tuplas programacion funcional

---
+++ Memoization/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Memoization: Es un proceso de optimizacion que tiene la informatica, 
+donde te ayuda a ahorrarte calculos, este concepto esta muy ligado
+al concepto de funcion pura.
+
+    Funcion pura: Una funcion pura tiene dos propiedades, si tu metes
+    un grupo de parametros, y este grupo siempre es el mismo el 
+    resultado no tiene porque cambiar, la segunda propiedad es que
+    una funcion pura no tiene efectos colaterales o cambios en efectos
+    externos a la funcion, si tu recibes un objeto este objeto no debe
+    de ser mutado, si vas a hacer un cambio debes de crear un nuevo
+    objeto y regresar ese nuevo objeto
+
+Aqui la memoization nos ayuda a que si tienes una funcion pura, es 
+recomendable ahorrarte ese calculo guardandolo en la memoria cache
+para poder acceder al resultado desde esa cache y optimizar el proceso
+para reducir tiempo y ahorrarte ese tiempo*/
+
+using System.Collections.Concurrent;
+using System.Diagnostics;
+
+namespace MemoizatedProject {
+    static class Program {
+        static void Main() {
+            Func<long, long> factorial = null;
+            factorial = (num) 
+                => num > 1 ? num * factorial(num - 1) : 1;
+
+
+            // NORMAL
+            var stopWatch1 = Stopwatch.StartNew();
+
+            for (int i = 1; i <= 1_000_000; i++)
+                factorial(9);
+
+            Console.WriteLine(stopWatch1.ElapsedMilliseconds);
+
+            // MEMORIZADO
+            var factorial2 = factorial.Memoize();
+            var stopWatch2 = Stopwatch.StartNew();
+
+            for (int i = 1; i <= 1_000_000; i++)
+                factorial2(9);
+
+            Console.WriteLine(stopWatch2.ElapsedMilliseconds);
+        }
+
+        public static Func<T, TResult> Memoize<T, TResult>(this Func<T, TResult> func) {
+            var cache = new ConcurrentDictionary<T, TResult>();
+            return (a) => cache.GetOrAdd(a, func);
+        }
+    }
+}
---
+++ TuplasYProgramacionFuncional/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace TuplasProgramacionFuncional {
+    class Program {
+        static void Main() {
+            // Execute1();
+            Execute2();
+        }
+
+        public static void Execute1() {
+            var counter = Counter();
+
+            counter.increment();
+            counter.increment();
+
+            Console.WriteLine(counter.getState());
+
+            counter.substract();
+
+            Console.WriteLine(counter.getState());
+        }
+
+        public static void Execute2() {
+            var beer = Beer();
+
+            beer.Counter.increment();
+            beer.Counter.increment();
+
+            Console.WriteLine(beer.Counter.getState());
+
+            beer.BeerDB.Add(10);
+        }
+
+        // Esto es composicion
+        public static (
+            (Action increment, Action substract, Func<int> getState) Counter,
+            (Action<int> Add, Action<int> Update, Action<int> Delete) BeerDB
+        ) Beer() {
+            return (
+                Counter(),
+                BeerDB()
+            );
+        }
+
+        public static (Action increment, Action substract, Func<int> getState) Counter() {
+            int i = 0;
+
+            void increment() => i++;
+            Action substract = () => i--; // Es igual al de arriba
+            Func<int> getState = () => i;
+
+            return (increment, substract, getState);
+        }
+
+        public static (Action<int> Add, Action<int> Update, Action<int> Delete) BeerDB() {
+            return (
+                (num) => Console.WriteLine("Agregada " + num),
+                (num) => Console.WriteLine("Editada " + num),
+                (num) => Console.WriteLine("Eliminada " + num)
+            );
+        }
+    }
+}
