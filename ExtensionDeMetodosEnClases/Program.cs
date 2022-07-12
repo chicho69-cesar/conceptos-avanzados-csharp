cea96a9a08e0e994e60f0d6c0e54f30cbd565707
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-07-12 09:55:42 -0500

    Temas agregados

---
+++ ExtensionDeMetodosEnClases/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+var name = "Cesar";
+var base64 = name.EncodeBase64();
+var decode64 = base64.DecodeBase64();
+
+Console.WriteLine(name);
+Console.WriteLine(base64);
+Console.WriteLine(decode64);
+
+public static class StringExtensions {
+    public static string EncodeBase64(this string value) {
+        var plainTextBytes = Encoding.UTF8.GetBytes(value);
+        return Convert.ToBase64String(plainTextBytes);
+    }
+
+    public static string DecodeBase64(this string value) {
+        var base64EncodedBytes = Convert.FromBase64String(value);
+        return Encoding.UTF8.GetString(base64EncodedBytes);
+    }
+}
---
+++ MultiplesFunciones/Program.cs
@@ -0,0 +1,17 @@
+﻿Func<int, int, int> fn = (int a, int b) => {
+    Console.WriteLine("Ejecuta 1");
+    return a * b;
+};
+
+fn += (int a, int b) => {
+    Console.WriteLine("Ejecuta 2");
+    return a + b;
+};
+
+fn += (int a, int b) => {
+    Console.WriteLine("Ejecuta 3");
+    return a - b;
+};
+
+var res = fn(15, 6);
+Console.WriteLine("El resultado es {0}", res);
