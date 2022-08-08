bfda2a29c99d6d644561f105d4ba8167ec652ff5
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-08-07 23:24:25 -0500

    Use the dynamic types

---
+++ Dynamic/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Los tipos de datos dinamicos o dynamic, son como su nombre lo indica
+tipos de datos que pueden cambiar su tipo de dato dinamicamente*/
+
+dynamic var1 = 1;
+Console.WriteLine(var1);
+
+var1 = "Hola Mundo";
+Console.WriteLine(var1);
+
+var1 = true;
+Console.WriteLine(var1);
