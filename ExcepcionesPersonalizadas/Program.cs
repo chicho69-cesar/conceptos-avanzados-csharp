d27c6a1f197ff2ba141ec6b84709965c8ac55bb4
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-08-07 23:33:41 -0500

    Add Personal Exceptions

---
+++ ExcepcionesPersonalizadas/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ExcepcionesPersonalizadas;
+
+public class Program {
+    public static void Main(string[] args) {
+        int x = 11;
+
+        try {
+            Test(x);
+        } catch (PersonalException e) {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    public static void Test(int x) {
+        if (x % 2 == 0) {
+            Console.WriteLine("El numero es par");
+        } else {
+            throw new PersonalException("El numero no es par bro :(");
+        }
+    }
+}
+
+public class PersonalException : Exception {
+    public PersonalException() { }
+    public PersonalException(string message) : base(message) { }
+}
