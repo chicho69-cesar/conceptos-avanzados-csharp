d9d7d8d86a876d0fb4a589ec1c2ccdeb9d8f8f0e
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-01-09 21:16:26 -0600

    Tareas Concurrentes con Expresiones Lambda

---
+++ ConcurrenciaConExpresionesLambda/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace ConcurrenciaConLambdas {
+    class Program {
+        static void Main(string[] args) {
+            var watch = Stopwatch.StartNew();
+            var tasks = new List<Task>();
+
+            for (int i = 0; i < 100; i++) {
+                tasks.Add(Task.Factory.StartNew(
+                    async () => {
+                        var client = new HttpClient();
+                        var response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/" + i);
+                        var result = await response.Content.ReadAsStringAsync();
+
+                        Console.WriteLine(result);
+                    }
+                ));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            watch.Stop();
+
+            Console.WriteLine(watch.ElapsedMilliseconds);
+            Console.ReadKey();
+        }
+    }
+}
